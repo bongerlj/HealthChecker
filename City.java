8dca770f2321f0858df8a4bbe042dad783ed0b1c
deanvanheukelom <37187120+deanvanheukelom@users.noreply.github.com>
2018-03-08 12:00:33 -0500

    added original city and point files

---
+++ City.java
@@ -0,0 +1,36 @@
+package sortPack;
+import java.io.*;
+
+public class City {
+	private int N = 25;
+	
+	private String cityName;
+	private int population;
+	private Point pos;
+	private int [] cond = new int [N];
+
+	City(String cityName, int population){
+		this.cityName=cityName;
+		this.population = population;
+	}
+	private String getCity(){
+		return cityName;
+	}
+	public int [] getCond(){
+		return cond;
+	}
+	public int getCond(int n){
+		return cond [n];
+	}
+	public void setCond(int [] cond){
+		this.cond=cond;
+	}
+	public void setPos(String l){
+		//will have to loop through in main program b\c there is no random access to a single line in file
+		String[] posString = l.split(",");
+		pos = new Point (Integer.parseInt(posString[0]), Integer.parseInt(posString[1]));
+	}
+	public Point getPos(){
+		return pos;
+	}
+}
---
+++ Point.java
@@ -0,0 +1,15 @@
+package sortPack;
+
+public class Point {
+	private int x, y;
+	Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public int x() {
+		return x;
+	}
+	public int y() {
+		return y;
+	}
+}
